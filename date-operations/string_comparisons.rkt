3b1a5b4c130e37189e0018f866f983af84df16ec
Zhiyang Ong <zhiyang.ong@tamu.edu>
2021-06-30 07:24:49 -0500

    Add string comparison operations, with references.

---
+++ date-operations/string_comparisons.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+
+#|
+Reference:
++ Rosetta Code contributors, "Date format: Haskell," from Rosetta Code, June 8, 2021. Available online from Rosetta Code: Explore: Programming Tasks category: D: Date format: \S47 Haskell at: https://www.rosettacode.org/wiki/Date_format#Racket; June 30, 2021 was the last access date.
+|#
+
+
+
+
+(require srfi/19)
+
+(print "Hello, World!")
+(println "Ciao Mondo!!!")
+
+
+
+; (let ((date (seconds->date (current-seconds))))
+;   (display (date-month month date year)))
+
+
+
+;;; The first required format is an ISO-8601 year-month-day format, predefined
+;;; as ~1 in date->string
+(displayln (date->string (current-date) "~1"))
+ 
+;;; You should be able to see how each of the components of the following format string
+;;; work...
+;;; ~d is zero padded day of month:
+(displayln (date->string (current-date) "~A, ~B ~d, ~Y"))
+;;; ~e is space padded day of month:
+(displayln (date->string (current-date) "~A, ~B ~e, ~Y"))
+; Get the date in the format: Month Day.
+(displayln (date->string (current-date) "~B ~e"))
+#|
+  References for string equality comparisons.
+  \cite[\S4.4.2 String Comparisons]{Flatt2021a}
+  Rosetta Code contributors, "Substring: Haskell: Strings," from Rosetta Code, June 8, 2021. Available online from Rosetta Code: Explore: Programming Tasks category: S: Substring: Haskell: Strings at: https://rosettacode.org/wiki/Substring#Haskell; June 29, 2021 was the last access date.
+|#
+(string=? "June 30" "June 30")
+(string=? "June 29" "June 30")
+(string=? "June 10" "June 11")
+
